7ef25704f0dbb229b19f3a4db63afaa7d54e1455
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-13 15:28:02 -0800

    created a working app and tests

--- detail/detail.js
+++ detail/detail.js
@@ -7,3 +7,11 @@ const dogDetailContainer = document.getElementById('dog-detail-container');
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
+window.addEventListener('load', async() => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+    const dog = await getDog(id);
+    const dogDetailEl = renderDogDetail(dog);
+    dogDetailContainer.append(dogDetailEl); 
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,41 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderDogCard, renderDogDetail } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderDogCard should return an anchor tag', (expect) => {
     //Arrange
+    let dog =
+        {
+            name: 'spot',
+            breed: 'corgi',
+            id: 1,
+        };
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<a href="./detail/?id=1"><div class="dog-card"><p>spot</p><img src="./assets/corgi.jpeg"></div></a>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderDogCard(dog);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
+});
+
+test ('renderDogDetail should retun a DOM element', (expect) => {
+    let dog =
+        {
+            name: 'spot',
+            breed: 'corgi',
+            age: 2,
+            id: 1,
+        };
+
+    const expected = '<div class="dog-detail"><p class="name">spot</p><img src="../assets/corgi.jpeg"><div class="age-and-breed"><p class="age">2 years old</p><p class="breed">corgi</p></div><p class="description"></p></div>';
+
+    const actual = renderDogDetail(dog);
+
+    expect.equal(actual.outerHTML, expected, 'renderMushroom() returns a DIV');
 });
